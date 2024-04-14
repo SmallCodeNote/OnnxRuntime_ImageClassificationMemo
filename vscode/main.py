d4b17a40445690879939cb0d2755794b20ce262e
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2024-04-15 05:08:42 +0900

    Add files via upload

--- vscode/main.py
+++ vscode/main.py
@@ -1,13 +1,16 @@
+import os
 import torch
 from torchvision import datasets, models, transforms
 import torch.onnx
+import onnx
+import onnxruntime
 
 if __name__ == '__main__':
     trainImageDirPath = R"R:\train"  # ここにディレクトリのパスを指定してください
     num_epoch_max = 10  # 実行予定エポック数を指定してください
-    resultOnnxPath = R"R:\best_model.onnx"  # ONNXファイルのパスを指定してください
+    resultOnnxPath = R"R:\model\best_model.onnx"  # ONNXファイルのパスを指定してください
+    resultPthPath = R"R:\model\best_model.pth"  # ONNXファイルのパスを指定してください
 
-    # データの前処理
     data_transforms = {
         'train': transforms.Compose([
             transforms.RandomResizedCrop(224),
@@ -17,20 +20,19 @@ if __name__ == '__main__':
         ]),
     }
 
-    # 画像の読み込み
+    # LoadImage
     image_datasets = {x: datasets.ImageFolder(trainImageDirPath, data_transforms[x]) for x in ['train']}
     dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=4) for x in ['train']}
-
-    # モデルの設定
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = models.mobilenet_v2(pretrained=True)
-    model = model.to(device)
 
-    # 学習の設定
+    if os.path.exists(resultPthPath):
+        model = torch.load(resultPthPath).to(device)
+    else:
+        model = models.mobilenet_v2(pretrained=True).to(device)
+
+    # Train
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
-
-    # 学習
     best_loss = float('inf')
     for epoch in range(num_epoch_max):
         running_loss = 0.0
@@ -51,7 +53,8 @@ if __name__ == '__main__':
         epoch_loss = running_loss / len(dataloaders['train'].dataset)
         print(f'Epoch {epoch+1}/{num_epoch_max}, Loss: {epoch_loss}')
 
-        # lossが最小になった状態のモデルを保存
+        # modelSave
         if epoch_loss < best_loss:
             best_loss = epoch_loss
-            torch.onnx.export(model, torch.randn(1, 3, 224, 224).to(device), resultOnnxPath,input_names="inputTensor",output_names="outputArray")
+            torch.onnx.export(model, torch.randn(1, 3, 224, 224).to(device), resultOnnxPath, input_names=["inputTensor"], output_names=["outputArray"])
+            torch.save(model, resultPthPath)
